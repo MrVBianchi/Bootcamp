c298be1c00f2ad5b84d98e5d3635bd8d50bac96e
Valentin <valenbian17@gmail.com>
2024-06-13 11:02:06 +0200

    haciendo funcionar el boton resultado

--- modulo-5/src/main.ts
+++ modulo-5/src/main.ts
@@ -1,110 +1,3 @@
-/*
-const generaNumAl = (): number => Math.floor(Math.random() * 101);
-const NumparaAcertar: number = generaNumAl();
-console.log(NumparaAcertar);
-
-type Estado =
-  | "NO_ES_UN_NUMERO"
-  | "EL_NUMERO_ES_MAYOR"
-  | "EL_NUMERO_ES_MENOR"
-  | "ES_EL_NUMERO_SECRETO"
-  | "GAME_OVER_MAXIMO_INTENTOS";
-
-const MAXIMO_INTENTOS = 5;
-let numeroDeIntentos = 0;
-const HasSuperadoElNumeroMaximoDeIntentos = (): boolean =>
-  numeroDeIntentos > MAXIMO_INTENTOS;
-
-const muestraNumeroDeIntentos = () => {
-  const elementoIntentos = document.getElementById("intentos");
-  if (elementoIntentos) {
-    elementoIntentos.innerHTML = `${numeroDeIntentos} de ${MAXIMO_INTENTOS}`;
-  } else {
-    console.error(
-      "muestraNumeroDeIntento: No se ha encontrado el elemento con id intentos"
-    );
-  }
-};
-document.addEventListener("DOMContentLoaded", muestraNumeroDeIntentos);
-
-const gestionarGameOver = (estado: Estado) => {
-  if (estado === "GAME_OVER_MAXIMO_INTENTOS") {
-    const elementoComprobar = document.getElementById("comprobar");
-    if (elementoComprobar && elementoComprobar instanceof HTMLButtonElement) {
-      elementoComprobar.disabled = true;
-    } else {
-      console.error(
-        "gestionarGameOver: No se ha encontrado el elemento con id comprobar"
-      );
-    }
-  }
-};
-const muestraMensajeComprobacion = (texto: string, estado: Estado) => {
-  let mensaje: string = "";
-  switch (estado) {
-    case "NO_ES_UN_NUMERO":
-      mensaje = `"${texto}" no es un número 🤨, prueba otra vez`;
-      break;
-    case "EL_NUMERO_ES_MAYOR":
-      mensaje = `UUYYY ! El número ${texto} es MAYOR que el número secreto`;
-      break;
-    case "EL_NUMERO_ES_MENOR":
-      mensaje = `UUYYY ! El número ${texto} es MENOR que el número secreto`;
-      break;
-    case "GAME_OVER_MAXIMO_INTENTOS":
-      mensaje = `🪦 GAME OVER, has superado el número máximo de intentos`;
-      break;
-    case "ES_EL_NUMERO_SECRETO":
-      mensaje = `¡¡¡Enhorabuena, has acertado el número!!! 🎉🎉🎉`;
-      break;
-    default:
-      mensaje = "No se que ha pasado, pero no deberías estar aquí";
-      break;
-  }
-  const elementoResultado = document.getElementById("resultado");
-  if (elementoResultado) {
-    elementoResultado.innerHTML = mensaje;
-  } else {
-    console.error(
-      "muestraMensajeComprobacion: No se ha encontrado el elemento con id resultado"
-    );
-  }
-};
-
-const comprobarNumero = (texto: string) => {
-  const numero = parseInt(texto);
-  const esUnNumero = !isNaN(numero);
-
-  if (!esUnNumero) {
-    return "NO_ES_UN_NUMERO";
-  }
-  if (numero === NumparaAcertar) {
-    return "ES_EL_NUMERO_SECRETO";
-  }
-
-  if (HasSuperadoElNumeroMaximoDeIntentos()) {
-    return "GAME_OVER_MAXIMO_INTENTOS";
-  }
-  return numero > NumparaAcertar ? "EL_NUMERO_ES_MAYOR" : "EL_NUMERO_ES_MENOR";
-};
-const handleCompruebaClick = () => {
-  let texto: string = "";
-  const inputElement = document.getElementById("numero");
-  if (inputElement && inputElement instanceof HTMLInputElement) {
-    texto = inputElement.value;
-  }
-  const estado: Estado = comprobarNumero(texto);
-  muestraMensajeComprobacion(texto, estado);
-  numeroDeIntentos++;
-  muestraNumeroDeIntentos();
-  gestionarGameOver(estado);
-};
-const botonComprobar = document.getElementById("comprobar");
-if (botonComprobar && botonComprobar instanceof HTMLButtonElement) {
-  botonComprobar.addEventListener("click", handleCompruebaClick);
-}
-*/
-
 // Juego 7 1/2
 
 // esta es la variable de la puntuacion
@@ -270,10 +163,10 @@ const gestionarGameOver = (puntuacion: number) => {
 };
 //mesajes comprobacion
 const comprobarPuntuacion = (numero: number): Estado => {
-  if (numero < 4) {
+  if (numero <= 4.5) {
     return `Menor_Cuatro`;
   }
-  if (numero === 5) {
+  if (numero >= 5 && numero < 6) {
     return `Num_cinco`;
   }
   if (numero === 6) {
@@ -290,6 +183,12 @@ const comprobarPuntuacion = (numero: number): Estado => {
 const muestraMensajeMePlanto = () => {
   const estado = comprobarPuntuacion(puntuacion);
   muestraMensajeComprobacion(estado);
+  if (botonResultado && botonResultado instanceof HTMLButtonElement) {
+    botonResultado.disabled = false;
+  }
+  if (botonDameCarta && botonDameCarta instanceof HTMLButtonElement) {
+    botonDameCarta.disabled = true;
+  }
 };
 //funcion reset juego
 
@@ -298,8 +197,14 @@ const resetGame = () => {
   muestraPuntuacion(puntuacion);
   cambiarScr(puntuacion);
   muestraMensajeComprobacion(`cero`);
+  const elementodameCarta = document.getElementById("dameCarta");
+  if (elementodameCarta && elementodameCarta instanceof HTMLButtonElement) {
+    elementodameCarta.disabled = false;
+  }
+  if (botonResultado && botonResultado instanceof HTMLButtonElement) {
+    botonResultado.disabled = true;
+  }
 };
-const cartaInicial = () => {};
 //boton dame carta llama a funcion
 const botonDameCarta = document.getElementById("dameCarta");
 if (botonDameCarta && botonDameCarta instanceof HTMLButtonElement) {
@@ -318,3 +223,9 @@ if (botonReset && botonReset instanceof HTMLButtonElement) {
 document.addEventListener("DOMContentLoaded", () => {
   muestraPuntuacion(puntuacion);
 });
+// boton muestra resultado para mostrar una carta una vez terminada
+const botonResultado = document.getElementById("muestraResultado");
+if (botonResultado && botonResultado instanceof HTMLButtonElement) {
+  botonResultado.disabled = true;
+  botonResultado.addEventListener("click", sumaCarta);
+}
